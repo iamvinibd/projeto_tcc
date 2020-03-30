2b6f31c7ad85b0b6f2138240b66aebdf52b637b7
iamvinibd <vinicius.brito96@hotmail.com>
2020-03-30 14:51:01 -0300

    Added style for slides // Linked Style slides with home.php

---
+++ home.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang=pt-br>
+  <head>
+    <meta charset="utf-8">
+    <title>Página Inicial - Login</title>
+    <link rel="stylesheet" href="_css/slide_style.css">
+  </head>
+  <body>
+    <div class="c_Slides">
+      <div class="SlideShow Fade">
+  			<img src="_img/_slides/001.jpg" class="SlideImage">
+  		</div>
+  		<div class="SlideShow Fade">
+  			<img src="_img/_slides/002.jpg" class="SlideImage">
+  		</div>
+  		<div class="SlideShow Fade">
+  			<img src="_img/_slides/003.jpg" class="SlideImage">
+  		</div>
+  		<div class="SlideShow Fade">
+  			<img src="_img/_slides/004.jpg" class="SlideImage">
+  		</div>
+      <div class="c_Login">
+      		<form method="get" action="UserLogin.php" id="LoginForm">
+      			<table id="LoginTable">
+      				<tr>
+      					<td>CPF</td>
+      					<td><input type="text" maxlength="11" name="UserCPF" class="TextInput"></td>
+      				</tr>
+      				<tr>
+      					<td>Senha</td>
+      					<td><input type="password" maxlength="6" name="UserSenha" class="TextInput"></td>
+      				</tr>
+      				<tr>
+      					<td colspan="2"><input type="submit" value="Entrar" class="btn"></td>
+      				</tr>
+      				<tr>
+      					<td colspan="2"><a href="PageCadastro.php"><input type="button" value="Cadastre-se" class="btn"></a></td>
+      				</tr>
+      			</table>
+      		</form>
+      	</div>
+
+    </div>
+  </body>
+</html>
