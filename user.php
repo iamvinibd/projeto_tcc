ba1a47cfd552f514e0f018a1609fe0ef6775b77d
iamvinibd <vinicius.brito96@hotmail.com>
2020-03-30 21:02:33 -0300

    Added to page user a way to list all contentes inside '_notas' folder with a HTML context

--- user.php
+++ user.php
@@ -1,5 +1,33 @@
-<?php
-echo "Workss";
-session_start();
-echo "$_SESSION[UserCPF]";
-?>
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <meta charset="utf-8">
+    <title>Página do Usuário</title>
+  </head>
+  <body>
+<div>
+  <table>
+    <?php
+    session_start();
+    echo "$_SESSION[UserCPF]";
+    $path = "./_notas/$_SESSION[UserCPF]/";
+    $Directory = new DirectoryIterator($path);
+    foreach ($Directory as $FileInfo) {
+      if($FileInfo -> isDot())continue;
+      $FileName = $FileInfo -> getFilename();
+/*
+      echo "</br>";
+      echo "$FileName";
+*/    $FilePath = "$path$FileName";
+      echo "$FilePath";
+
+      ?>
+
+      <a target="_blank" href="<?=$FilePath?>" ><p><?=$FileName?></p></a><br/>
+    <?php  }?>
+
+
+
+    </div>
+  </body>
+</html>
