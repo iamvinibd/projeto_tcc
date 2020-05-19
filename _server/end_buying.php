38a611b648ad922c46a275c7bc88acadfa2cfeee
iamvinibd <vinicius.brito96@hotmail.com>
2020-05-19 12:52:35 -0300

    added PDF generator

--- _server/end_buying.php
+++ _server/end_buying.php
@@ -1,16 +1,87 @@
 <?php
   session_start();
-  // Include the main TCPDF library (search for installation path).
-  require_once('../TCPDF/tcpdf.php');
-
+  $CPF = $_SESSION["UserCPF"];
+  $nome = $_SESSION["UserName"];
+  $email = $_SESSION["UserEmail"];
   $now = getdate();
+
   $mday = $now['mday'] - ($now['wday'] + 6) % 7;
   $monday = mktime(0, 0, 0, $now['mon'], $mday, $now['year']);
   $date = date('d_m_y', $monday).".json";
-  echo $date;
 
-  /*$CPF = $_SESSION["UserCPF"];
-  $pdf = pdf_new();
-  pdf_open_file($pdf,"./_notas/$_SESSION[UserCPF]/PDF/AEEE")
-*/
+  $date2 = date('d_m_y', $monday);
+  $filename = "../_notas/$_SESSION[UserCPF]/JSON/$date";
+  $path_to_save ="C:/xampp/htdocs/projeto_tcc/_notas/$CPF/PDF/Nota_Fiscal_$date2.pdf";
+  //echo $path_to_save;
+  require_once('../TCPDF/tcpdf.php');
+  // create new PDF document
+  $pdf = new TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
+  // Set font
+  // dejavusans is a UTF-8 Unicode font, if you only need to
+  // print standard ASCII chars, you can use core fonts like
+  // helvetica or times to reduce file size.
+  $pdf->SetFont('dejavusans', '', 14, '', true);
+  $pdf->setPrintHeader(false);
+  $pdf->setPrintFooter(false);
+  // Add a page
+  // This method has several options, check the source code documentation for more information.
+  $pdf->AddPage();
+  // Set some content to print
+  $html = <<<EOD
+  <h1>IPCart - Nota Fiscal </h1>
+  <h4>Dados do cliente</h4>
+  <p>$nome</p>
+  <p>$CPF</p>
+  <p>$email</p>
+  <table>
+  <tr>
+    <td>Código</td>
+    <td>Produto</td>
+    <td>Quantidade</td>
+    <td>Valor</td>
+  </tr>
+  EOD;
+  //$pdf->writeHTMLCell(0, 0, '', '', $html, 0, 1, 0, true, '', true);
+  if (file_exists($filename)){
+    //echo $filename;
+    //echo "<br>";
+    $count = substr_count(file_get_contents($filename), "a");
+    //echo $count;
+    //echo "<br>";
+    //echo $count;
+    if ($count > 0){
+      //echo "TO WeakMap";
+      $data = file_get_contents($filename); // put the contents of the file into a variable
+      $characters = json_decode($data,true);
+      $valor_total = 0;
+      foreach ($characters as $character) {
+        $valor_total = strval($valor_total + intval($character["info"][0]["qtdd"])*intval($character["info"][0]["valor"]));
+        $valor_produto = strval(intval($character["info"][0]["qtdd"])*intval($character["info"][0]["valor"]));
+        $codigo = $character["codigo"];
+        $produto = $character["info"][0]["produto"];
+        $qtdd = $character["info"][0]["qtdd"];
+        $html .= <<<EOD
+          <tr>
+            <td>$codigo</td>
+            <td>$produto</td>
+            <td>$qtdd</td>
+            <td>$valor_produto,00</td>
+          </tr>
+
+  EOD;
+    }
+    $html .= <<<EOD
+        <tr>
+          <td>Total</td>
+          <td colspan = 3>$valor_produto,00</td>
+          </tr>
+      </table>
+      EOD;
+  }
+}
+  $pdf->writeHTMLCell(0, 0, '', '', $html, 0, 1, 0, true, '', true);
+  // Close and output PDF document
+  // This method has several options, check the source code documentation for more information.
+  $pdf->Output($path_to_save, 'F');
+
 ?>
